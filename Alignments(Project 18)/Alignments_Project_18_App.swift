25f8de59048eca388c4d65913026b612b44e8095
BeaverOnAMission <babenko.bohdan01@gmail.com>
2024-03-03 09:10:20 +0200

    Initial Commit

---
+++ Alignments(Project 18)/Alignments_Project_18_App.swift
@@ -0,0 +1,17 @@
+//
+//  Alignments_Project_18_App.swift
+//  Alignments(Project 18)
+//
+//  Created by mac on 04.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct Alignments_Project_18_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ScrollEffectsContentView()
+        }
+    }
+}
